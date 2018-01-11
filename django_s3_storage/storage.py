dcf79523e26277d86a569597b4120ed7efb384a9
Dave Hall <dave@etianen.com>
2018-01-11 10:14:52 +0000

    Another attempt to fix multithreading.

--- django_s3_storage/storage.py
+++ django_s3_storage/storage.py
@@ -107,7 +107,8 @@ class _Local(local):
             connection_kwargs["aws_session_token"] = storage.settings.AWS_SESSION_TOKEN
         if storage.settings.AWS_S3_ENDPOINT_URL:
             connection_kwargs["endpoint_url"] = storage.settings.AWS_S3_ENDPOINT_URL
-        self.s3_connection = boto3.client("s3", config=Config(
+        self.session = boto3.session.Session()
+        self.s3_connection = self.session.client("s3", config=Config(
             s3={"addressing_style": storage.settings.AWS_S3_ADDRESSING_STYLE},
             signature_version=storage.settings.AWS_S3_SIGNATURE_VERSION,
         ), **connection_kwargs)

c1b98486a83c4481668ee56f768d82c10b46dff1
Dave Hall <dave.hall@wayflyer.com>
2023-11-04 15:42:35 +0000

    Skiping file open text mode test on Python 3.10

--- tests/django_s3_storage_test/tests.py
+++ tests/django_s3_storage_test/tests.py
@@ -1,5 +1,7 @@
 import posixpath
+import sys
 import time
+import unittest
 from contextlib import contextmanager
 from datetime import timedelta
 from io import StringIO
@@ -86,6 +88,7 @@ class TestS3Storage(SimpleTestCase):
         with self.save_file(content="foo"):
             self.assertEqual(default_storage.open("foo.txt").read(), b"foo")
 
+    @unittest.skipIf(sys.version_info < (3, 12))
     def testOpenTextMode(self):
         with self.save_file(content="foo"):
             self.assertEqual(default_storage.open("foo.txt", "r").read(), "foo")
